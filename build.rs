d6596b86b3b3e663a1d443c59da3ece9a870fcf1
TR Lim <trlim@allwinnerkorea.com>
2018-10-28 20:50:43 +0900

    Updrage to latest bindgen and fix cross compile problem

--- build.rs
+++ build.rs
@@ -1,20 +1,31 @@
 extern crate bindgen;
+extern crate cc;
 
 use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    let libyuv_dir = env::var("LIBYUV_DIR").expect("LIBYUV_DIR should be defined");
+    // In cross compiling, recent bindgen fails if proper `--sysroot` is not specified.
+    // To workaround this, try to figure out sysroot path from the compiler itself.
+    let mut compiler = cc::Build::new().get_compiler().to_command();
+    let sysroot = compiler.arg("-print-sysroot")
+        .output()
+        .map(|o| String::from_utf8(o.stdout).unwrap().trim().to_string())
+        .ok();
+
+    let mut bindgen = bindgen::Builder::default()
+        .header("wrapper.h")
+        .trust_clang_mangling(false);
+
+    if let Some(path) = sysroot {
+        println!("cargo:warning=Using library at {:?}", path);
+        bindgen = bindgen.clang_arg(format!("--sysroot={}", path));
+    }
+
+    let bindings = bindgen.generate()
+        .expect("Unable to generate bindings");
 
     println!("cargo:rustc-link-lib=yuv");
-    println!("cargo:rustc-link-search=native={}/lib", libyuv_dir);
-
-    let bindings = bindgen::Builder::default()
-        .clang_arg(format!("-I{}/include", libyuv_dir))
-        .header("wrapper.h")
-        .trust_clang_mangling(false)
-        .generate()
-        .expect("Unable to generate bindings");
 
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings.write_to_file(out_path.join("bindings.rs"))
