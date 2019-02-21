4aaad4660c0bd08af6767a1d9fccb73c562ea864
TR Lim <trlim@allwinnerkorea.com>
2019-02-21 17:22:14 +0900

    Change build script to use bundled feature

--- ci/script.sh
+++ ci/script.sh
@@ -4,18 +4,18 @@ set -ex
 
 # TODO This is the "test phase", tweak it as you see fit
 main() {
-    cross build --target $TARGET
-    cross build --target $TARGET --release
+    cross build --target $TARGET --features "bundled"
+    cross build --target $TARGET --features "bundled" --release
 
     if [ ! -z $DISABLE_TESTS ]; then
         return
     fi
 
-    cross test --target $TARGET
-    cross test --target $TARGET --release
+    cross test --target $TARGET --features "bundled"
+    cross test --target $TARGET --features "bundled" --release
 
-    cross run --target $TARGET
-    cross run --target $TARGET --release
+    cross run --target $TARGET --features "bundled"
+    cross run --target $TARGET --features "bundled" --release
 }
 
 # we don't run the "test phase" when doing deploys
